22d0ea473329dc0dc10e928f1cf9f3b310180624
JuanMonLluch <juanmontillalluch98@gmail.com>
2022-01-23 22:25:37 +0100

    - Refs in UIController.cs - Prefab canvasUIController modifier

--- Assets/Scripts/InGame/UIController.cs
+++ Assets/Scripts/InGame/UIController.cs
@@ -1,9 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
+
+    [Header("Texts")]
+    public TextMeshProUGUI rondaText;
+    public TextMeshProUGUI cronoText;
+    public TextMeshProUGUI mensajeInicioRonda;
+
+    [Header("Buttons")]
+    public Button exitButton;
+
+    private void Awake()
+    {
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
