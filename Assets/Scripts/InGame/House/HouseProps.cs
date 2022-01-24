97915126dd97809a076a367eb7c483f826c138dd
Alexander Cirac Antio <95984829+AlexanderCirac@users.noreply.github.com>
2022-01-24 22:03:19 +0100

    news updates for  task HouseProps
    Size  ScenePrefab  correctly and add RCP to HouseProps.cs and change enum to a List<>

--- Assets/Scripts/InGame/House/HouseProps.cs
+++ Assets/Scripts/InGame/House/HouseProps.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Photon.Pun;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,24 +10,63 @@ using UnityEngine;
           [Header("Name")]
           public string _nameObject;
           [Header("Enum")]
-          public HousePropType _housePropEnum;
+          public List<HousePropType> _roomType;
           [Header("Point")]
           public int _amountPoints;
           [Header("Size")]
-          public Vector2 _vectSize;
+          public Vector2 _baseSize;
           [Header("Bools")]
           public bool _floorObject;
           public bool _wallObject;
           internal bool _objetctPicked;
+          private bool _objetctPickedCheck;
+          private bool _endCorrutine;
+          // Singlentons
+          [System.NonSerialized]
+          public PhotonView photonView;
           #endregion
           
           #region UnityCalls
           void Start()
           {
-
+              StartCoroutine(CorrutineUpdate());
+          }
+          private void OnDestroy()
+          {
+              _endCorrutine = true;
           }
           #endregion
 
           #region Methods
+          IEnumerator CorrutineUpdate()
+          {
+              while (!_endCorrutine)
+              {   
+                  if(_objetctPicked != _objetctPickedCheck && !_objetctPickedCheck)
+                  {
+                      photonView.RPC(nameof(RPCPickedTrue), RpcTarget.Others);
+                      _objetctPickedCheck = true;
+                  }
+                  if (_objetctPicked != _objetctPickedCheck && _objetctPickedCheck)
+                  {
+                      photonView.RPC(nameof(RPCPickedFalse), RpcTarget.Others);
+                      _objetctPickedCheck = false;
+                  }
+                  yield return null;
+              }
+          } 
+          #endregion
+
+          #region RPCs
+          [PunRPC]
+          void RPCPickedTrue()
+          {
+              _objetctPicked = true;
+          }          
+          [PunRPC]
+          void RPCPickedFalse()
+          {
+              _objetctPicked = false;
+          }
           #endregion
     }
