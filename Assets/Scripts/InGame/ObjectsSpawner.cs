5296ad1eefec918b87345b1f101c37b8411b0110
Santiago Dopazo <elmundoesazul@gmail.com>
2022-01-25 06:41:14 +0100

    Detail parent objects

--- Assets/Scripts/InGame/ObjectsSpawner.cs
+++ Assets/Scripts/InGame/ObjectsSpawner.cs
@@ -8,6 +8,7 @@ public class ObjectsSpawner : MonoBehaviour
 {
     [SerializeField] HouseProps[] objectList;
     [SerializeField] Vector3 size;
+    [SerializeField] float objectOffser = 0.1f;
     
     // Start is called before the first frame update
     void Awake()
@@ -25,11 +26,12 @@ public class ObjectsSpawner : MonoBehaviour
             for (int z = 0; currentPos.z < initPos.z + size.z + size.x; z++)
             {
                 int rnd = Random.Range(0, objectList.Length);
-                currentPos += objectList[rnd]._baseSize.y * Vector3.forward;
+                currentPos += (objectList[rnd]._baseSize.y + objectOffser) * Vector3.forward;
                 biggerX = objectList[rnd]._baseSize.x > biggerX ? objectList[rnd]._baseSize.y : biggerX;
-                Instantiate(objectList[rnd], currentPos + (Vector3.right * (biggerX / 2)), objectList[rnd].transform.rotation);
+                HouseProps prop = Instantiate(objectList[rnd], currentPos + (Vector3.right * (biggerX / 2)), objectList[rnd].transform.rotation);
+                prop.transform.parent = this.transform;
             }
-            currentPos = new Vector3(currentPos.x + biggerX, transform.position.y, initPos.z);
+            currentPos = new Vector3(currentPos.x + biggerX + objectOffser, transform.position.y, initPos.z);
         }
     }
 
